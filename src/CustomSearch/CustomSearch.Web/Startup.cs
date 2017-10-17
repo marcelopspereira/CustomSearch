4521b7aeee0598a3c870770e978b1eeec53b72cb
Fabricio Catae <fcatae@microsoft.com>
2017-10-17 16:35:37 -0200

    add index

--- src/CustomSearch/CustomSearch.Web/Startup.cs
+++ src/CustomSearch/CustomSearch.Web/Startup.cs
@@ -43,7 +43,7 @@ namespace CustomSearch.Web
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Search}/{action=Index}/{id?}");
             });
         }
     }
