3779ba04981c96b538ca8f27a94caabfa6bb9c77
Daniel Semedo <danpin@microsoft.com>
2017-10-27 10:54:08 -0200

    final commit

---
+++ src/CustomSearch/CustomSearch.AzureSearchAPI/Database.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE SearchWebPages
+(
+       [Id]                       INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+       [Title]                    NVARCHAR(1000) NOT NULL,
+       [Description]				  NVARCHAR(MAX) NOT NULL,
+       [Category]                 NVARCHAR(500) NULL,
+       [Link]                     VARCHAR(1000) NOT NULL,
+       [Keywords]                 NVARCHAR(1000) NULL,
+       [Modified]                 ROWVERSION,
+       [Deleted]                  BIT NOT NULL DEFAULT(0)
+)
+
+
+drop PROC procAddSearchWebPages
+
+CREATE PROC procAddSearchWebPages(@title NVARCHAR(1000), @description NVARCHAR(MAX), @link VARCHAR(1000), @category NVARCHAR(500), @keywords NVARCHAR(1000))
+AS
+SET NOCOUNT ON
+UPDATE SearchWebPages SET [Title]=@title, [Description]=@description, [Category]=@category, [Keywords]=@keywords WHERE [Link]= @link;
+IF @@ROWCOUNT = 0 
+       INSERT SearchWebPages([Title], [Description], [Link], [Category], [Keywords]) VALUES (@title, @description, @link, @category, @keywords);
+
+CREATE INDEX idxLink ON SearchWebPages(Link)
+
+INSERT SearchWebPages([Title],[Description],[Link],[Category],[Keywords],[Deleted])
+       SELECT [Title],[Title],[Link],@keywords,0 FROM WebPages
